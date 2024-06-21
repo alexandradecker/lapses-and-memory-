96eb4184f4aa2707033111fe4e5243d43ab85124
alexandradecker <45908966+alexandradecker@users.noreply.github.com>
2024-06-20 22:47:36 -0400

    Delete info.r

--- info.r
+++
@@ -1,7 +0,0 @@
-#### Exclusions ####
-
-outlier <- data.table(participant = c(163, 201, 20), exclusion= c("low task accuracy", "low task accuracy","large sustained attention score"))
-
-info <- list()
-info$memoryHitsDifferences<- list("memory_hits" = 'zero assigned to low confidnece in correct responses', 
-                                  'memory_hits_dprime' = "NA assigned to all low confidnece hits regardless of accuracy")
